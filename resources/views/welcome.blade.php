63b2be1ef324a4a52f4609a85ec6e11ca7008c65
shintan777 <tanvi.shinde46@gmail.com>
2019-10-15 21:51:36 +0530

    removed gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,8 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+               
+
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
