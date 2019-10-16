ebeda09155ba84bbfe1421e3040f421e0a7b2763
Aditya Srivastava <adityasrivastava301199@gmail.com>
2019-10-16 15:46:52 +0530

    home route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,8 +64,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('edit-profile') }}">
-                                   Edit Profile
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                   View Profile
                                  </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
